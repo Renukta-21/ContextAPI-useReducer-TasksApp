b7f6f07aaefef4efd0b2c46ff2a53a27b7edfaaf
Daniel Urbina <edu211004@gmail.com>
2024-12-11 15:12:53 -0600

    initialState changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 import { useReducer } from "react"
 
 function App() {
+  const initialState = {
+    tasks:[
+      {
+        id: '1',
+        name: 'Task 1',
+        description: 'This is a description for Task 1.'
+      },
+      {
+        id: '2',
+        name: 'Task 2',
+        description: 'This is a description for Task 2.'
+      },
+      {
+        id: '3',
+        name: 'Task 3',
+        description: 'This is a description for Task 3.'
+      }
+    ]
+    
+  };
+  
   const tasksReducer = (state, action) => {
     switch (action.type) {
       case 'ADD_TASK':
@@ -10,12 +31,22 @@ function App() {
         return state
     }
   }
-  const [state, dispatch] = useReducer(tasksReducer, [])
-  
+  const [state, dispatch] = useReducer(tasksReducer, initialState)
+
   return (
     <div>
       <h2>Task App</h2>
-      <p>{state.length}</p>
+      {state.tasks.map(t=>(
+        <div key={t.id}>
+          <small>{t.id}</small>
+          <h3>{t.name}</h3>
+          <p>{t.description}</p>
+          <button>DELETE</button>
+          {/* <button>EDIT</button> */}
+          <hr />
+        </div>
+      ))}
+
     </div>
   )
 }
