75eaafb7ce8eb52727c62d6e1de7fe67f877563c
Daniel Urbina <edu211004@gmail.com>
2024-12-11 14:57:50 -0600

    Defining reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useReducer } from "react"
+
 function App() {
+  const tasksReducer = (state, action) => {
+    switch (action.type) {
+      case 'ADD_TASK':
+        return 0
+
+      default:
+        return state
+    }
+  }
+  const [state, dispatch] = useReducer(tasksReducer, [])
+  
   return (
     <div>
       <h2>Task App</h2>
+      <p>{state.length}</p>
     </div>
   )
 }
