1aefb9efc30a395313476790eeff827f6627dd43
Daniel Urbina <edu211004@gmail.com>
2024-12-11 18:03:59 -0600

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { Routes,BrowserRouter, Route } from 'react-router'
 import AddTask from './components/AddTask'
+import Header from './components/Header'
 import TaskList from './components/TaskList'
 
-function App() {  
+function App() {
   return (
-      <div>
-      <h2>Task App</h2>
-      <AddTask />
-      <TaskList/>
+    <div>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+         <Route path='/' element={ <TaskList />}/>
+         <Route path='/add-Task' element={ <AddTask />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
-
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div>
+        <h2>Task App 📓 </h2>
+    </div>
+  )
+}
+
+export default Header
